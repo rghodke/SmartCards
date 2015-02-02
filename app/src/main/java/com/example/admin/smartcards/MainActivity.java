db9c5e9031719f3e575c23bc26affbd93ece6505
Ranjit Ghodke <rghodke@ucsd.edu>
2015-02-02 14:39:57 -0800

    Added Orientation Preferences
    Also ironed out a few crash bugs
    Also removed title bar for fullscreen experience

--- app/src/main/java/com/example/admin/smartcards/MainActivity.java
+++ app/src/main/java/com/example/admin/smartcards/MainActivity.java
@@ -1,16 +1,19 @@
 package com.example.admin.smartcards;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     EditText username = null;
     EditText password = null;
@@ -18,7 +21,6 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
     }
 
