05f965af5b49dc4081fde8681074b7f4fa1a548f
Ranjit Ghodke <rghodke@ucsd.edu>
2015-02-04 12:30:29 -0800

    Added CreateDeckBack View
    Added the CreateDeckBack View

--- app/src/main/java/com/example/admin/smartcards/CreateCardBack.java
+++ app/src/main/java/com/example/admin/smartcards/CreateCardBack.java
@@ -6,44 +6,39 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
-public class CreateCard extends Activity {
+public class CreateCardBack extends Activity {
+
+    TextView ViewTitle;
+    EditText Classtitle;
+    EditText Entertitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_card);
+        setContentView(R.layout.activity_create_card_back);
+        ViewTitle = (TextView) findViewById(R.id.ViewTitle);
+        Classtitle = (EditText) findViewById(R.id.EnterCourse);
+        Entertitle = (EditText) findViewById(R.id.EnterTitle);
+
+        String ctitle = "CHECK";
+
+                        //Toast.makeText(getApplicationContext(), value,
+                          //  Toast.LENGTH_SHORT).show();
+
+                //Classtitle.getText().toString();
+        ViewTitle.setText(ctitle);
     }
 
-
-    public void goBack (View view)
-    {
-        Intent intent = new Intent(CreateCard.this, DeckList.class);
-        startActivity(intent);
-    }
-
-
-    public void createBack (View view)
-    {
-
-
-        Intent intent = new Intent(CreateCard.this, CreateCardBack.class);
-        startActivity(intent);
-    }
-
-    public void goDone (View view)
-    {
-    //    Intent intent = new Intent(CreateCard.this, DeckList.class);
-    //    startActivity(intent);
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_create_card, menu);
+        getMenuInflater().inflate(R.menu.menu_create_card_back, menu);
         return true;
     }
 
